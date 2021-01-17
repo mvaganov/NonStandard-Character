284d6b2c2ec0419c3e5224c963212857abb915a4
Michael Vaganov <michael.vaganov@gmail.com>
2021-01-16 20:53:18 -0800

    button access

---
+++ Assets/NonStandard/Scripts/Utility/OnActiveChange.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnActiveChange : MonoBehaviour {
+	public UnityEvent onEnable, onDisable;
+	void OnEnable() { onEnable.Invoke(); }
+	void OnDisable() { onDisable.Invoke(); }
+}
